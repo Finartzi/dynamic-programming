16b481a82f996536d3fd7aeb530774411f594174
Arto Päivinen <arto.pai@luukku.com>
2022-02-24 23:15:41 +0200

    gridTraveler memoization

--- grid-traveler.js
+++ grid-traveler.js
@@ -7,14 +7,20 @@
 
 */
 
-const gridTraveler = (n, m) => {
+const gridTraveler = (n, m, memo = {}) => {
+    const key = m + "," + n;
+
+    // are the args in the memo?
+    if (key in memo) return memo[key];
     if(m === 1 && n === 1) return 1;
     if(m === 0 || n === 0) return 0;
-    return gridTraveler(m - 1, n) + gridTraveler(m, n - 1);
+    
+    memo[key] = gridTraveler(m - 1, n, memo) + gridTraveler(m, n - 1, memo);
+    return memo[key];
 };
 
 console.log(gridTraveler(1, 1));
 console.log(gridTraveler(2, 3));
 console.log(gridTraveler(3, 2));
 console.log(gridTraveler(3, 3));
-console.log(gridTraveler(15, 15));
+console.log(gridTraveler(25, 25));
