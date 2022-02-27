fc45590e0e29818e3864a6df90ac4507a12a7d56
Arto Päivinen <arto.pai@luukku.com>
2022-02-27 13:57:13 +0200

    Count-Construct memoized

--- count-construct.js
+++ count-construct.js
@@ -7,17 +7,19 @@
 
 */
 
-const countConstruct = (target, wordBank) => {
+const countConstruct = (target, wordBank, memo = {}) => {
+    if(target in memo) return memo[target];
     if(target === "") return 1;
 
     let totalCount = 0;
     for(let word of wordBank){
         if(target.indexOf(word) === 0){
-            const numWaysForRest = countConstruct(target.slice(word.length), wordBank);
+            const numWaysForRest = countConstruct(target.slice(word.length), wordBank, memo);
             totalCount += numWaysForRest;
         }
     }
-    return totalCount;
+    memo[target] = totalCount;
+    return memo[target];
 };
 
 console.log(countConstruct("purple", ["purp", "p", "ur", "le", "purpl"]));
