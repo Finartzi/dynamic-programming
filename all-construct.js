04c90bf5b93d8acd57719ba31550e0ba71b834bd
Arto Päivinen <arto.pai@luukku.com>
2022-02-28 23:46:22 +0200

    All-Construct

---
+++ all-construct.js
@@ -0,0 +1,37 @@
+/* 
+    Dynamic Programming by @freeCodeCamp.org
+
+    <https://www.youtube.com/watch?v=oBt53YbR9Kk>
+
+    All-Construct
+
+*/
+
+const allConstruct = (target, wordBank) => {
+    if(target === "") return [[]];
+
+    const result = [];
+
+    for(let word of wordBank){
+        if(target.indexOf(word) === 0){
+            const suffix = target.slice(word.length);
+            const suffixWays = allConstruct(suffix, wordBank);
+            const targetWays = suffixWays.map( way => [ word, ...way ]);
+            result.push(...targetWays);
+        }
+    }
+    return result;
+};
+
+console.log(allConstruct("purple", ["purp", "p", "ur", "le", "purpl"]));
+console.log(allConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"]));
+console.log(allConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"]));
+console.log(allConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"]));
+console.log(allConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",
+    ["e",
+    "ee",
+    "eee",
+    "eeee",
+    "eeeee",
+    "eeeeee"
+]));
